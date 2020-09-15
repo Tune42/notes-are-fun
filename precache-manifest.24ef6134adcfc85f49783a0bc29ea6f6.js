28a229e970cf57a5956c0e29d5cd875e5291cd97
Tune42 <59482059+Tune42@users.noreply.github.com>
2020-09-14 22:59:47 -0500

    Updates

---
+++ precache-manifest.24ef6134adcfc85f49783a0bc29ea6f6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "513c2f7b3272167675afe8ad4bcef610",
+    "url": "/notes-are-fun/index.html"
+  },
+  {
+    "revision": "91c282763c14026eeb37",
+    "url": "/notes-are-fun/static/css/main.708f5913.chunk.css"
+  },
+  {
+    "revision": "bd73ccacae6f731a530e",
+    "url": "/notes-are-fun/static/js/2.5982e1ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/notes-are-fun/static/js/2.5982e1ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91c282763c14026eeb37",
+    "url": "/notes-are-fun/static/js/main.7d3c5f8f.chunk.js"
+  },
+  {
+    "revision": "b5078709de533ee9ce49",
+    "url": "/notes-are-fun/static/js/runtime-main.42b079c3.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/notes-are-fun/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/notes-are-fun/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/notes-are-fun/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/notes-are-fun/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/notes-are-fun/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
